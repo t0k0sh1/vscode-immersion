daa86d45988a75a593d76684492bbc5f2dac79c8
Takashi Yamashina <takashi.yamashina@gmail.com>
2021-03-27 09:20:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Immersion Theme
+
+Immersion theme is a minimal themes for immersing yourself in programming.
+
+## License
+
+MIT license
---
+++ test/python.py
@@ -0,0 +1,26 @@
+# comment
+from collections import deque
+
+
+def topo(G, ind=None, Q=[1]):
+    """
+    docstring
+    """
+    if ind == None:
+        ind = [0] * (len(G) + 1)  # this is a comment
+        for u in G:
+            for v in G[u]:
+                ind[v] += 1
+        Q = deque()
+        for i in G:
+            if ind[i] == 0:
+                Q.append(i)
+    if len(Q) == 0:
+        return
+    v = Q.popleft()
+    print(v)
+    for w in G[v]:
+        ind[w] -= 1
+        if ind[w] == 0:
+            Q.append(w)
+    topo(G, ind, Q)
